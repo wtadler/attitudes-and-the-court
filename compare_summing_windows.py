464a3f5f292c94fdf63356d355b910d64dcd8d27
Alex Rich <alexandersrich@gmail.com>
2016-04-28 12:04:26 -0400

    add lines to summing window plots for no court
    shows how un-useful the court data is as we are currently modeling it
    :/

--- compare_summing_windows.py
+++ compare_summing_windows.py
@@ -15,6 +15,8 @@ gss = processing.preprocess_gss()
 # gss = processing.preprocess_gss(composite=None, extra_imports=['affrmact'])
 
 
+train_nocourt_R2 = 0.0
+test_nocourt_R2 = 0.0
 train_R2 = np.array([0.0] * 10)
 test_R2 = np.array([0.0] * 10)
 windows = range(1, 11)
@@ -29,13 +31,21 @@ for l in range(n_reps):
         model = modeling.fit_model(train, test, y_col, x_cols, x_cols_nocourt, print_fit=False)
         train_R2[i-1] = train_R2[i-1] + model[0].score(train[:, x_cols], train[:, y_col])
         test_R2[i-1] = test_R2[i-1] + model[0].score(test[:, x_cols], test[:, y_col])
+        if i == 1:
+            train_nocourt_R2 = train_nocourt_R2 + model[1].score(train[:, x_cols_nocourt], train[:, y_col])
+            test_nocourt_R2 = test_nocourt_R2 + model[1].score(test[:, x_cols_nocourt], test[:, y_col])
 
 train_R2 = train_R2 / n_reps
 test_R2 = test_R2 / n_reps
+train_nocourt_R2 = train_nocourt_R2 / n_reps
+test_nocourt_R2 = test_nocourt_R2 / n_reps
+
 
 plt.figure()
 plt.plot(windows, train_R2, label="train")
 plt.plot(windows, test_R2, label="test")
+plt.plot([windows[0], windows[-1]], [train_nocourt_R2, train_nocourt_R2], label="train - no court")
+plt.plot([windows[0], windows[-1]], [test_nocourt_R2, test_nocourt_R2], label="test - no court")
 plt.legend()
 plt.xlabel("court summing window")
 plt.ylabel("R^2")
